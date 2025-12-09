b53e9966efa5a9c08875cbe5eeab4be464535ddb
Jirasak Songsri <Jirasak.S.Guy@gmail.com>
2025-12-09 09:33:03 +0700

    เพิ่มไปใช้ env

--- Frontend/app/lib/data.ts
+++ Frontend/app/lib/data.ts
@@ -9,7 +9,7 @@ import { Operation, OperationDependency } from "@/app/types/Operation";
 import { Routing } from "@/app/types/Routing";
 import { ProductionOrder, WorkCenterSchedule } from "@/app/types/Production";
 
-const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 // =====================================================
 // HELPER FUNCTION
